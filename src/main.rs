f45e6fc25e0cf279ad5157a52e4f445b784d66ad
WindProphet <lihanyuan1996@gmail.com>
2021-11-22 07:51:26 +0900

    recursive print json

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,35 @@ fn read_json_from_file(path: &str) -> Result<Value, Box<dyn Error>> {
     Ok(ser)
 }
 
+fn print_color_json(val: &Value, indent: usize) {
+    match val {
+        Value::Null => print!("null"),
+        Value::Bool(b) => print!("{}", b),
+        Value::Number(n) => print!("{}", n),
+        Value::String(s) => print!("{}", s),
+        Value::Array(a) => {
+            print!("[\n");
+            for el in a {
+                let repeated = " ".repeat(indent + 2);
+                print!("{}", repeated);
+                print_color_json(el, indent + 2);
+                print!(",\n");
+            }
+            print!("{}]", " ".repeat(indent));
+        }
+        Value::Object(o) => {
+            print!("{{\n");
+            for (key, val) in o {
+                let repeated = " ".repeat(indent + 2);
+                print!("{}{}: ", repeated, key);
+                print_color_json(val, indent + 2);
+                print!(",\n");
+            }
+            print!("{}}}", " ".repeat(indent));
+        }
+    }
+}
+
 fn main() {
     let matches = App::new("JSON Editor")
         .version(env!("CARGO_PKG_VERSION"))
@@ -19,8 +48,8 @@ fn main() {
         .arg(Arg::new("FILE").about("input file"))
         .get_matches();
     if let Some(i) = matches.value_of("FILE") {
-        match read_json_from_file(&i) {
-            Ok(ser) => println!("{:#?}", ser),
+        match read_json_from_file(i) {
+            Ok(ser) => print_color_json(&ser, 0),
             Err(err) => println!("{}", err),
         };
     }
